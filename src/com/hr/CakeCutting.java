40fa07e802713407867de4fc4704df31c8db8837
Nandakishore K <nandakishorek17@gmail.com>
2016-02-08 23:05:20 -0500

    TODO: cakecutting problem

---
+++ src/com/hr/CakeCutting.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.hr;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/contests/bitwise2k14/challenges/cake-cutting
+ * 
+ * @author kishore
+ *
+ */
+public class CakeCutting {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int R = sc.nextInt();
+            int r = sc.nextInt();
+            
+        }
+        sc.close();
+    }
+
+}
